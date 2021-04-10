e5852a585a26c3a3958b09760966c85cba97f6a1
Dayson <coderontheinternet@tutamail.com>
2021-04-10 10:21:50 +0800

    Update question file

--- lib/question.dart
+++ lib/question.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text('asdf');
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10.0),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
